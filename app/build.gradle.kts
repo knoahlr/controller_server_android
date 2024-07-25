427fd652c633840de48c152ed1e454b8c315605a
root <root@shadesmar>
2024-07-25 09:02:23 -0400

    initial commit. Server 2. Kotlin

---
+++ app/build.gradle.kts
@@ -0,0 +1,72 @@
+val kotlin_version: String by project
+val logback_version: String by project
+
+plugins {
+    alias(libs.plugins.android.application)
+    alias(libs.plugins.jetbrains.kotlin.android)
+    //id("io.ktor.plugin") version "2.3.12"
+}
+
+android {
+    namespace = "com.example.server_2"
+    compileSdk = 34
+
+    defaultConfig {
+        applicationId = "com.example.server_2"
+        minSdk = 24
+        targetSdk = 34
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+    kotlinOptions {
+        jvmTarget = "1.8"
+    }
+    buildFeatures {
+        viewBinding = true
+    }
+}
+
+dependencies {
+
+    implementation(libs.androidx.core.ktx)
+    implementation(libs.androidx.appcompat)
+    implementation(libs.material)
+    implementation(libs.androidx.constraintlayout)
+    implementation(libs.androidx.navigation.fragment.ktx)
+    implementation(libs.androidx.navigation.ui.ktx)
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.androidx.junit)
+    androidTestImplementation(libs.androidx.espresso.core)
+
+/*
+    //androidTestImplementation("androidx.compose.ui:ui-test-junit4")
+    debugImplementation("androidx.compose.ui:ui-test-manifest")
+    implementation("io.ktor:ktor-server-core")
+    implementation("io.ktor:ktor-server-websockets")
+    implementation("io.ktor:ktor-network-tls")
+    implementation("io.ktor:ktor-server-netty")
+    implementation("ch.qos.logback:logback-classic:$logback_version")
+    testImplementation("io.ktor:ktor-server-tests")
+*/
+
+    implementation("io.ktor:ktor-server-core")
+    implementation("io.ktor:ktor-server-netty")
+
+    //testImplementation("org.jetbrains.kotlin:kotlin-test-junit:$kotlin_version")
+}
