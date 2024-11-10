5c05a8d66143f26dbe865fbc961ec743033de5e8
denjj <knoah.lr@gmail.com>
2024-11-10 17:07:54 -0500

    feat: new home page

--- app/src/main/java/com/example/server_2/HomeFragment.kt
+++ app/src/main/java/com/example/server_2/HomeFragment.kt
@@ -1,36 +1,41 @@
 package com.example.server_2
 
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import androidx.navigation.fragment.findNavController
-import com.example.server_2.databinding.FragmentFirstBinding
+import com.example.server_2.databinding.FragmentSecondBinding
+import com.example.server_2.ServerActivity
+import com.example.server_2.databinding.FragmentHomeBinding
 
 /**
- * A simple [Fragment] subclass as the default destination in the navigation.
+ * A simple [Fragment] subclass as the second destination in the navigation.
  */
-class FirstFragment : Fragment() {
+class HomeFragment : Fragment() {
 
-    private var _binding: FragmentFirstBinding? = null
+    private var _binding: FragmentHomeBinding? = null
 
+    // This property is only valid between onCreateView and
     private val binding get() = _binding!!
-    val server: ServerActivity = ServerActivity(9999)
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        _binding = FragmentFirstBinding.inflate(inflater, container, false)
+        _binding = FragmentHomeBinding.inflate(inflater, container, false)
         return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        binding.buttonFirst.setOnClickListener {
-            //findNavController().navigate(R.id.action_FirstFragment_to_SecondFragment)
+        binding.ServerButton.setOnClickListener {
+            findNavController().navigate(R.id.action_HomeFragment_to_SecondFragment)
         }
     }
 
